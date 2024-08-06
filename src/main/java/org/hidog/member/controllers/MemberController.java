5a23ed6ecd9cc66c283c19b61f40820453646cd2
sunkyu0384 <sunkyu0384@naver.com>
2024-08-06 20:11:51 +0900

    회원정보 주소 구성 중

--- src/main/java/org/hidog/member/controllers/MemberController.java
+++ src/main/java/org/hidog/member/controllers/MemberController.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.hidog.member.validators.JoinValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
