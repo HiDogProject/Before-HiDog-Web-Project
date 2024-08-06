87061d5b3a9d36dbc75c20247c3da6a97eef6069
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-06 16:58:52 +0900

    Fix: templates 유틸 메서드 형식으로 변경

--- src/main/java/org/hidog/map/controllers/MapController.java
+++ src/main/java/org/hidog/map/controllers/MapController.java
@@ -1,4 +1,18 @@
 package org.hidog.map.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/map")
+@RequiredArgsConstructor
 public class MapController {
+
+    @GetMapping("/main")
+    public String join() {
+
+        return "front/map/tMap";
+    }
 }
