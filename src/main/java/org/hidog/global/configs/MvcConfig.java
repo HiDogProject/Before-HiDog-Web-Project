71fbef1f4fd8c1d298af668ab7a8fadf2e6e8a52
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-08-06 15:24:47 +0900

    no message

--- src/main/java/org/hidog/global/configs/MvcConfig.java
+++ src/main/java/org/hidog/global/configs/MvcConfig.java
@@ -1,5 +1,6 @@
 package org.hidog.global.configs;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing //Spring boot 애플리케이션 JPA 감독(auditing) 기능 활성화합니다.
+@EnableDiscoveryClient
 public class MvcConfig implements WebMvcConfigurer {
 
 
---
+++ src/main/java/org/hidog/map/controllers/MapController.java
@@ -0,0 +1,4 @@
+package org.hidog.map.controllers;
+
+public class MapController {
+}
