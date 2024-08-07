fb1211438255027da0a963fc0102233489eb43b5
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 18:13:50 +0900

    RequestProfile 커맨드 객체 수정

--- src/main/java/org/hidog/mypage/controllers/RequestProfile.java
+++ src/main/java/org/hidog/mypage/controllers/RequestProfile.java
@@ -1,12 +1,26 @@
 package org.hidog.mypage.controllers;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RequestProfile {
+
+    @NotBlank @Email
+    private String email;
+
+    @NotBlank @Size(min = 8, max = 20)
+    private String password;
+
+    @NotBlank @Size(min = 2, max = 10)
+    private String nickName;
+
     @NotBlank
-    private String id; // Member의 id
+    private String address1;
+
+    private String address2;
 
     private String profileImage; // 프로필 이미지
 }
