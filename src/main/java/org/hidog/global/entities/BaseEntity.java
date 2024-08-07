690a8a9c3b30a6e4275e9d004a4e411c50f28ca3
parksaehyun <psh666777@naver.com>
2024-08-07 21:14:58 +0900

    feat: 보드 공통헤더 구성중, 보드설정 엔티티, 보드데이터 엔티티 구성중
    보드 공통헤더 구성중, 보드설정 엔티티, 보드데이터 엔티티 구성중

--- src/main/java/org/hidog/global/entities/BaseEntity.java
+++ src/main/java/org/hidog/global/entities/BaseEntity.java
@@ -1,6 +1,32 @@
 package org.hidog.global.entities;
 
-import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-public class BaseEntity implements Serializable {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@MappedSuperclass // 공통속성화 클래스이다 알려주는 애노테이션
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+    @CreatedDate
+    @Column(updatable = false) // 수정불가 // 처음가입일자는 수정 못하게 해야함
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false) // 추가불가, 수정할 때 값 추가 가능
+    private LocalDateTime modifiedAt;
+
+    @Column(insertable = false) // 추가불가, 수정할 때 값 추가 가능
+    private LocalDateTime deletedAt; // 일정시간이 지나면 자동 삭제
 }
