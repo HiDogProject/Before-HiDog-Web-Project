2b54571730886bf7c4696b3eb6152bf3da8a8c81
sunkyu0384 <sunkyu0384@naver.com>
2024-08-08 14:08:21 +0900

    fix: 회원가입 버튼 클릭 시 form의 값이 requestJoin으로 넘어오지 않던 오류 수정

--- src/main/java/org/hidog/member/controllers/MemberController.java
+++ src/main/java/org/hidog/member/controllers/MemberController.java
@@ -4,6 +4,7 @@ package org.hidog.member.controllers;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hidog.global.exceptions.ExceptionProcessor;
 import org.hidog.member.services.MemberSaveService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,21 +14,80 @@ import org.springframework.web.bind.annotation.*;
 import org.hidog.member.validators.JoinValidator;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 @SessionAttributes("requestLogin")
-public class MemberController {
+public class MemberController implements ExceptionProcessor {
 
     private final JoinValidator joinValidator;
     private final MemberSaveService memberSaveService;
 
+    @ModelAttribute
+    public RequestLogin requestLogin() {
+        return new RequestLogin();
+    }
+
+    @GetMapping("/join")
+    public String join(@ModelAttribute RequestJoin form, Model model) {
+        commonProcess("join", model);
+
+
+        return "front/member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinPs(@Valid RequestJoin form, Errors errors, Model model) {
+        commonProcess("join", model);
+
+        joinValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            return "front/member/join";
+        }
+
+        memberSaveService.save(form);
+
+        return "redirect:/member/login";
+    }
+    /**
+     * 회원 관련 컨트롤러 공통 처리
+     *
+     * @param mode
+     * @param model
+     */
+    private void commonProcess(String mode, Model model) {
+        mode = Objects.requireNonNullElse(mode, "join");
+
+        List<String> addCss = new ArrayList<>();
+        List<String> addCommonScript = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+
+        addCss.add("member/style");  // 회원 공통 스타일
+        if (mode.equals("join")) {
+            addCommonScript.add("fileManager");
+            addCss.add("member/join");
+            addScript.add("member/join");
+
+        } else if (mode.equals("login")) {
+            addCss.add("member/login");
+        }
+
+        model.addAttribute("addCss", addCss);
+        model.addAttribute("addCommonScript", addCommonScript);
+        model.addAttribute("addScript", addScript);
+    }
+}
+    /*
     @GetMapping("/join")
     public String join(@ModelAttribute RequestJoin form, Model model) {
         model.addAttribute("addScript", List.of("joinAddress"));
+
         return "front/member/join";
     }
 
@@ -36,23 +96,13 @@ public class MemberController {
 
         joinValidator.validate(form, errors);
 
-
         if (errors.hasErrors()) {
             return "front/member/join";
         }
 
         memberSaveService.save(form); // 회원 가입 처리
 
-
         return "redirect:/member/login";
     }
 
-    @GetMapping("/login")
-    public String login(@Valid @ModelAttribute RequestLogin form, Errors errors) {
-        String code = form.getCode();
-        if (StringUtils.hasText(code)) {
-            errors.reject(code, form.getDefaultMessage());
-        }
-        return "front/member/login";
-    }
-}
+     */
